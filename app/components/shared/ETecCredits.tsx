be1675922b48e3e38d9c0b3a37b67324d9041848
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-10-31 13:47:25 -0300

    feat: adicionar componentes modules e shared

---
+++ app/components/shared/ETecCredits.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function ETecCredits() {
+    return (
+        <p className="max-w-3xl mx-auto leading-relaxed text-gray-700">
+            O curso utiliza como base o material{" "}
+            <em>“Inglês Módulo 1 - Caderno 1”</em>, produzido pelos professores{" "}
+            <strong>
+                Helton Bezerra Moreira, Juciana Maria Paulo e Lourival Soares de Aquino Filho
+            </strong>
+            , do <strong>Instituto Federal Sul-rio-grandense (IFSUL)</strong>, para a{" "}
+            <strong>Rede e-Tec Brasil</strong>.
+        </p>
+    );
+}
