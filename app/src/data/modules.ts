6b4e28aca9224eab713a962b6cbdc34636bda846
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-10-31 23:53:08 -0300

    feat: adicionar arquivo modules.ts

---
+++ app/src/data/modules.ts
@@ -0,0 +1,15 @@
+export const englishModules = [
+    { title: "Identificação e Caracterização Pessoal", link: "/identificacao-e-caracterizacao-pessoal" },
+    { title: "Transportes, Família e Rotina", link: "/transport-family-routine" },
+    { title: "Rotina, Alimentação e Vestuário", link: "/rotina-alimentacao-vestuario" },
+    { title: "Restaurante, Saúde e Lazer", link: "/restaurante-saude-lazer" },
+    { title: "Adjetivos e Viagens", link: "/adjetivos-viagens-revisao" },
+    { title: "Condicional e Mídia", link: "/conditionals-and-media" },
+    { title: "Canada & Gramática Avançada", link: "/canada-money-holidays-grammar" },
+    { title: "Esportes, Funerais e Acadêmico", link: "/esportes-funerais-academico" },
+    { title: "Filmes, Histórias e Vida", link: "/movies-stories-life" },
+    { title: "Causative, Tag & Indirect", link: "/causative-tag-indirect" },
+    { title: "Trabalho e Modal Verbs", link: "/trabalho-modais-adjetivos" },
+    { title: "Future, Modals & Past", link: "/future-modals-past" },
+    { title: "Clima, Esportes e Habilidades", link: "/condicoes-clima-esportes-habilidades" }
+];
