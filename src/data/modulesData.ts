fa819e04d4a453d32c27603139a4a1566710f52a
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:50:05 -0300

    feat: adicionar arquivo modulesData.ts

---
+++ src/data/modulesData.ts
@@ -0,0 +1,94 @@
+import { ModuleItem } from "@/type/modulesType";
+
+export const modulesData: ModuleItem[] = [
+    {
+        title: "Personal Information & Introductions",
+        link: "/basic/personal-information-introductions",
+        level: "Básico",
+        icon: "user",
+    },
+    {
+        title: "Family, Transportation & Daily Life",
+        link: "/basic/family-transportation-daily-life",
+        level: "Básico",
+        icon: "users",
+    },
+    {
+        title: "Daily Routine, Food & Clothing",
+        link: "/basic/daily-routine-food-clothing",
+        level: "Básico",
+        icon: "clock",
+    },
+    {
+        title: "Restaurants, Health & Leisure",
+        link: "/basic/restaurants-health-leisure",
+        level: "Básico",
+        icon: "utensils",
+    },
+    {
+        title: "Adjectives & Travel Experiences",
+        link: "/basic/adjectives-travel-experiences",
+        level: "Básico",
+        icon: "plane",
+    },
+    {
+        title: "Conditionals & Media Language",
+        link: "/intermediate/conditionals-media-language",
+        level: "Intermediário",
+        icon: "newspaper",
+    },
+    {
+        title: "Countries, Culture & Advanced Grammar",
+        link: "/intermediate/countries-culture-advanced-grammar",
+        level: "Intermediário",
+        icon: "globe",
+    },
+    {
+        title: "Sports, Education & Academic Life",
+        link: "/intermediate/sports-education-academic-life",
+        level: "Intermediário",
+        icon: "trophy",
+    },
+    {
+        title: "Movies, Stories & Life Experiences",
+        link: "/intermediate/movies-stories-life-experiences",
+        level: "Intermediário",
+        icon: "film",
+    },
+    {
+        title: "Causative Form, Tag Questions & Indirect Speech",
+        link: "/advanced/causative-tag-questions-indirect-speech",
+        level: "Avançado",
+        icon: "messageSquare",
+    },
+    {
+        title: "Workplace English & Modal Verbs",
+        link: "/advanced/workplace-english-modal-verbs",
+        level: "Avançado",
+        icon: "cloudSun",
+    },
+    {
+        title: "Future Tenses, Modals & Past Review",
+        link: "/advanced/future-tenses-modals-past-review",
+        level: "Avançado",
+        icon: "calendar",
+    },
+    {
+        title: "Weather, Sports & Personal Skills",
+        link: "/advanced/weather-sports-personal-skills",
+        level: "Avançado",
+        icon: "cloudSun",
+    },
+    {
+        title: "Real-Life Conversations & Situations",
+        link: "/advanced/real-life-conversations-situations",
+        level: "Avançado",
+        icon: "messageCircle",
+    },
+    {
+        title: "English for Travel, Work & Daily Use",
+        link: "/advanced/english-for-travel-work-daily-use",
+        level: "Avançado",
+        icon: "compass",
+    },
+];
