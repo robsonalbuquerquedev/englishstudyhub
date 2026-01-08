355cb9f6787defa0f78134a66f652d8a6855343a
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:30:35 -0300

    feat: adicionar rota /intermediate/movies-stories-life-experiences

---
+++ app/intermediate/movies-stories-life-experiences/page.tsx
@@ -0,0 +1,7 @@
+import MoviesStoriesLifeExperiences from "@/components/topics/MoviesStoriesLifeExperiences";
+
+export default function MoviesStoriesLifeExperiencesPage() {
+    return (
+        <MoviesStoriesLifeExperiences />
+    )
+}
