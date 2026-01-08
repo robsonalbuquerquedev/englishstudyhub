43cf70bc50c87fb5dff9eb4cf096adad7489deab
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:22:42 -0300

    feat: adicionar rota /advanced/weather-sports-personal-skills

---
+++ app/advanced/weather-sports-personal-skills/page.tsx
@@ -0,0 +1,7 @@
+import WeatherSportsPersonalSkills from "@/components/topics/WeatherSportsPersonalSkills";
+
+export default function WeatherSportsPersonalSkillsPage() {
+    return (
+        <WeatherSportsPersonalSkills />
+    )
+}
