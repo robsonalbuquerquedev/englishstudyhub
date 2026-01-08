da3b0b47db4328e983fe2672a22b788106e73821
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:26:15 -0300

    feat: adicionar rota /basic/personal-information-introductions

---
+++ app/basic/personal-information-introductions/page.tsx
@@ -0,0 +1,7 @@
+import PersonalInformationIntroductions from "@/components/topics/PersonalInformationIntroductions";
+
+export default function PersonalInformationIntroductionsPage() {
+    return (
+        <PersonalInformationIntroductions />
+    )
+}
