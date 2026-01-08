0821ccfa4d9495d4c5f12b55fea34103655d94c9
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:37:59 -0300

    feat: adicionar componente modulesIcons.tsx

---
+++ src/components/icons/modulesIcons.tsx
@@ -0,0 +1,20 @@
+// components/icons/modulesIcons.tsx
+import { User, Users, Clock, Utensils, Plane, Newspaper, Globe, Trophy, Film, MessageSquare, Briefcase, Calendar, CloudSun, MessageCircle, Compass, } from "lucide-react";
+
+export const moduleIcons = {
+    user: User,
+    users: Users,
+    clock: Clock,
+    utensils: Utensils,
+    plane: Plane,
+    newspaper: Newspaper,
+    globe: Globe,
+    trophy: Trophy,
+    film: Film,
+    messageSquare: MessageSquare,
+    briefcase: Briefcase,
+    calendar: Calendar,
+    cloudSun: CloudSun,
+    messageCircle: MessageCircle,
+    compass: Compass,
+};
