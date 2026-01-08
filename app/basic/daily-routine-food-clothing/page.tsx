bc86dcb6f61ccd63b11c851fb1b18f477e910544
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-08 15:23:44 -0300

    feat: adicionar rota /basic/daily-routine-food-clothing

---
+++ app/basic/daily-routine-food-clothing/page.tsx
@@ -0,0 +1,7 @@
+import DailyRoutineFoodClothing from "@/components/topics/DailyRoutineFoodClothing";
+
+export default function DailyRoutineFoodClothingPage() {
+    return (
+        <DailyRoutineFoodClothing />
+    )
+}
