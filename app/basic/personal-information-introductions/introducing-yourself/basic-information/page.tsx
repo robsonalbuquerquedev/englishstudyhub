05c5e28cb88fc2ee09fde6ed99cac3f374596e67
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-10 15:58:24 -0300

    feat: adicionar rota basic/personal-information-introductions/introducing-yourself/basic-information

---
+++ app/basic/personal-information-introductions/introducing-yourself/basic-information/page.tsx
@@ -0,0 +1,7 @@
+import BasicInformation from "@/components/introducingyourself/BasicInformation";
+
+export default function BasicInformationPage() {
+    return (
+        <BasicInformation />
+    )
+}
