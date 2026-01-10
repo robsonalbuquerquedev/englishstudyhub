5bb45140e904a828dcb52dda9ca99c079cc51f7b
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-10 17:42:28 -0300

    feat: adicionar rota basic/personal-information-introductions/introducing-yourself/review-and-glossary

---
+++ app/basic/personal-information-introductions/introducing-yourself/review-and-glossary/page.tsx
@@ -0,0 +1,7 @@
+import QuickReviewAndGlossary from "@/components/introducingyourself/QuickReview&Glossary";
+
+export default function QuickReviewAndGlossaryIntroducingYourselPage() {
+    return (
+        <QuickReviewAndGlossary />
+    )
+}
